0f674ebcefa788d2c966f7ca312a62d59f7f2c1a
twoelevenjay <leon@211j.com>
2022-01-02 21:37:25 -0500

    Add plugin header

---
+++ defiantpress.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The plugin init file.
+ *
+ * @package DefiantPress
+ * @version 1.7.2
+ *
+ * Plugin Name: DefiantPress
+ * Plugin URI:  https://github.com/twoelevenjay/defiantpress
+ * Description: This is not just a plugin, it symbolizes the hope that Defiant will find the developer they need, and that developer would be me. When activated you will randomly see a Defiant team member in the upper right of your admin screen on every page. Although I took inspiration from the first WordPress plugin ever, "Hello Dolly", every line of code is original and authored by me.
+ * Author:      Leon Shelhamer
+ * Version:     1.0.0
+ * Author URI:  https://211j.com/
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ **/
