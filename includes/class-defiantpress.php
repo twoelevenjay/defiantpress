fdbc7b35923babc67dc46e8a57020a2849008d1b
twoelevenjay <leon@211j.com>
2022-01-02 23:04:36 -0500

    Create main plugin class

---
+++ includes/class-defiantpress.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The file that defines the core plugin class.
+ *
+ * @since   1.0.0
+ * @package DefiantPress
+ */
+
+/**
+ * The core plugin class.
+ *
+ * @since      1.0.0
+ * @package    DefiantPress
+ * @author     Leon <leon@211j.com>
+ */
+class DefiantPress {
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+
+		$this->get_team();
+	}
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function get_team() {
+
+	}
+
+}
