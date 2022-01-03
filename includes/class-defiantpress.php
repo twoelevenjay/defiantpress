7fcb05b4f55dbdf412ed5f8e75f77ad1163f3995
twoelevenjay <leon@211j.com>
2022-01-03 02:02:31 -0500

    Use Goutte dependancy to crawl team members

--- includes/class-defiantpress.php
+++ includes/class-defiantpress.php
@@ -6,6 +6,8 @@
  * @package DefiantPress
  */
 
+use Goutte\Client;
+
 /**
  * The core plugin class.
  *
@@ -14,6 +16,15 @@
  * @author     Leon <leon@211j.com>
  */
 class DefiantPress {
+
+	/**
+	 * Defiant team members crawled from https://www.defiant.com/team/.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      array    $members    Contains an array of all team members.
+	 */
+	protected $members = array();
 
 	/**
 	 * Define the core functionality of the plugin.
@@ -33,6 +44,21 @@ class DefiantPress {
 	 */
 	private function get_team() {
 
+		$client  = new Client();
+		$crawler = $client->request( 'GET', 'https://www.defiant.com/team/' );
+		$crawler->filter( 'div.staff-member' )->each(
+			function ( $node, $i ) {
+				$name_title      = $node->filter( 'h2' )->first()->html();
+				$name_title      = str_replace( '</span>', '', $name_title );
+				$name_title      = explode( '<span>', $name_title );
+				$this->members[] = array(
+					'name'  => $name_title[0],
+					'title' => $name_title[1],
+					'image' => $node->filter( 'img' )->first()->attr( 'src' ),
+					'about' => $node->children()->last()->text(),
+				);
+			}
+		);
 	}
 
 }
